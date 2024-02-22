7e1904223dcab589d0a83331e2d42fa4f0ab8a50
C3vd3t <63650334+cevdetkarakulak@users.noreply.github.com>
2024-02-22 14:27:51 +0300

    NKatmanliMimari Projesi Eklendi

---
+++ NkatmanliMimari/DataAccessLayer/DALPersonel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using EntitiyLayer;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccessLayer
+{
+    class DALPersonel
+    {
+        public static List<EntityPersonel> PersonelListesi()
+        {
+            List<EntityPersonel>degerler=new List<EntityPersonel>();
+            SqlCommand komut1 = new SqlCommand("Select * From TBLBILGI", Baglanti.bgl);
+            if (komut1.Connection.State!=ConnectionState.Open)
+            {
+                komut1.Connection.Open();
+            }
+            SqlDataReader dr=komut1.ExecuteReader();
+            while (dr.Read())
+            {
+                EntityPersonel ent = new EntityPersonel();
+                ent.Id = int.Parse(dr["ID"].ToString());
+                ent.Ad = dr["AD"].ToString();
+                ent.Soyad = dr["SOYAD"].ToString();
+                ent.Gorev = dr["GOREV"].ToString();
+                ent.Sehir = dr["SEHIR"].ToString();
+                ent.Maas = short.Parse( dr["AD"].ToString());
+                degerler.Add(ent);
+
+            }
+            dr.Close();
+            return degerler;
+        }
+
+    }
+}
---
+++ NkatmanliMimari/NkatmanliMimari/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace NkatmanliMimari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 324);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
